883ed67e7708883f21deb4acec55f3c66b2f909f
amir <amirtaki623@gmail.com>
2025-05-14 01:34:42 +0330

    stylesCSS : SHOW SCROLL UP & edit JS show scroll up

--- src/javaScript/app.js
+++ src/javaScript/app.js
@@ -52,7 +52,7 @@ window.addEventListener('scroll', bgHander )
 //  ---------------------- SHOW SCROLL UP --------------------------
 const scrollUP =  () =>{
     const scrollUP = window.document.getElementById("scroll-up")
-    this.scrollUP >= 350 ? scrollUP.classList.add("show-scroll") :
+    this.scrollY >= 350 ? scrollUP.classList.add("show-scroll") :
                            scrollUP.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUP)
